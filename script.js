3384f73b7aacb26c4dccaed28fe28d221e03cc56
Xhuli4 <xhuli.laci@gmail.com>
2025-02-06 14:56:51 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const list = document.querySelectorAll('.navList');
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click',activeLink));
